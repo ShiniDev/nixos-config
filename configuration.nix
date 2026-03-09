a89cba1191f04d373af8ab6dd95e9b0cbbde4b5b
shinidev <markjoefreylaurente@gmail.com>
2026-03-09 03:01:38 -0400

    Added wofi and wl-clipboard

--- configuration.nix
+++ configuration.nix
@@ -79,6 +79,8 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
 	git
+wofi
+wl-clipboard
   ];
 
 programs.sway.enable = true;
