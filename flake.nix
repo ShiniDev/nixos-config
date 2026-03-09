096c6a7c2826ed981c76c414eb84b6739ccae27b
shinidev <markjoefreylaurente@gmail.com>
2026-03-09 06:22:14 -0400

    Added flakes

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    {
+      nixosConfigurations.shinixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+      };
+    };
+}
