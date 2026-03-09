e46612542bdbe0e45242cabc22e81d5bdeab21fd
shinidev <markjoefreylaurente@gmail.com>
2026-03-09 02:55:24 -0400

    Added sway

--- configuration.nix
+++ configuration.nix
@@ -81,6 +81,8 @@
 	git
   ];
 
+programs.sway.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
