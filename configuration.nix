294ff0a4698527b164c2346a560917119cdead4d
shinidev <markjoefreylaurente@gmail.com>
2026-03-09 03:20:23 -0400

    Allowed unfree packages and added chrome and code.

--- configuration.nix
+++ configuration.nix
@@ -72,6 +72,7 @@
   };
 
   # programs.firefox.enable = true;
+ nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
@@ -81,7 +82,10 @@
 	git
 wofi
 wl-clipboard
+google-chrome # Unfree
+vscode # Unfree
   ];
+          
 
 programs.sway.enable = true;
 
