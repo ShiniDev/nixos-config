81dad8fde42b38db8c48c72c2a4345b053873ef9
shinidev <markjoefreylaurente@gmail.com>
2026-03-11 13:26:33 -0400

    Added dns

--- modules/networking.nix
+++ modules/networking.nix
@@ -2,4 +2,8 @@
 {
   # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
+  networking.networkmanager.appendNameservers = [
+    "1.1.1.1"
+    "8.8.8.8"
+  ];
 }
