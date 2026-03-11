ee807d2f903df5ee6e9d84a7d40379d042797c2e
shinidev <markjoefreylaurente@gmail.com>
2026-03-11 10:04:33 -0400

    Typo

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.shinidev = ./home/shinidev/;
+            home-manager.users.shinidev = ./home/shinidev;
           }
         ];
       };
