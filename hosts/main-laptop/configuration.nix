2f0e31acd8f8416dbefb02fdce560c3ab6addd8f
shinidev <markjoefreylaurente@gmail.com>
2026-03-16 00:33:26 -0400

    Added a system wide bash config

--- hosts/main-laptop/configuration.nix
+++ hosts/main-laptop/configuration.nix
@@ -12,6 +12,7 @@
     ../../modules/users.nix
     ../../modules/console.nix
     ../../modules/time.nix
+    ../../modules/bash.nix
   ];
   networking.hostName = "shinixos"; # Define your hostname.
   system.stateVersion = "25.11"; # DO NOT CHANGE
