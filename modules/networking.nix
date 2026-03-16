adcb181dfb8a019d7bad65cf6d0cbd5df7d7956e
shinidev <markjoefreylaurente@gmail.com>
2026-03-16 03:20:37 -0400

    Just depend on router dns

--- modules/networking.nix
+++ modules/networking.nix
@@ -2,8 +2,4 @@
 {
   # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
-  networking.networkmanager.appendNameservers = [
-    "1.1.1.1"
-    "8.8.8.8"
-  ];
 }
