253f3aab55852b9821af6962d58500b1d299b673
shinidev <markjoefreylaurente@gmail.com>
2026-03-19 13:14:03 -0400

    Added function shortcuts to hyprland

--- home/shinidev/hyprland.nix
+++ home/shinidev/hyprland.nix
@@ -33,6 +33,14 @@
         "SUPER Shift, 5, movetoworkspace, 5"
         "SUPER Shift, 6, movetoworkspace, 6"
       ];
+      bindel = [
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ",XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+"
+        ",XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-"
+      ];
     };
   };
   /*
