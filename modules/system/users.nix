02788027db6c930fa10b20fabfa51cdef54d7aa2
shinidev <markjoefreylaurente@gmail.com>
2026-03-21 12:31:43 -0400

    forgot to add .keys

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -5,7 +5,7 @@
     group = "shinidev";
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    openssh.authorizedKeys = [
+    openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ3+ZgIr4gkofsH5gQYKb60t2Stx5WWsQbhAzmW6Emxm shinidev@home-station
 "
     ];
