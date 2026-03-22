e87bc32380079882545a04d9ff7e5069e6b00d1d
shinidev <markjoefreylaurente@gmail.com>
2026-03-22 03:50:11 -0400

    Enabled home manager again

--- flake.nix
+++ flake.nix
@@ -3,11 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
       nixpkgs,
+      home-manager,
       ...
     }@inputs:
     {
@@ -16,6 +21,12 @@
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.shinidev = ./home.nix;
+          }
         ];
       };
     };
---
+++ home.nix
@@ -0,0 +1,9 @@
+{
+  ...
+}:
+{
+  home.username = "shinidev";
+  home.homeDirectory = "/home/shinidev";
+  home.stateVersion = "25.11";
+  programs.home-manager.enable = true;
+}
