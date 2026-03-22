67a77a3616e08c3ee8b96eea23ec739cc508647e
shinidev <markjoefreylaurente@gmail.com>
2026-03-22 04:58:27 -0400

    Added home-server configuration

---
+++ hosts/home-server/configuration.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/system/default.nix
+  ];
+  system.stateVersion = "25.11"; # DO NOT CHANGE
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  networking.hostName = "home-server"; # Define your hostname.
+  shinidev.networking.role = "desktop";
+
+}
