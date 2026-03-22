9f5f712f450829b4d15bf7aa8b8fa20a081afa9e
shinidev <markjoefreylaurente@gmail.com>
2026-03-22 04:54:15 -0400

    Forgot to add desktop modules

--- hosts/home-station/configuration.nix
+++ hosts/home-station/configuration.nix
@@ -6,6 +6,7 @@
   imports = [
     ./hardware-configuration.nix
     ../../modules/system/default.nix
+    ../../modules/desktop/default.nix
   ];
   system.stateVersion = "25.11"; # DO NOT CHANGE
   boot.kernelPackages = pkgs.linuxPackages_latest;
---
+++ modules/desktop/default.nix
@@ -0,0 +1,9 @@
+{...}:
+{
+	imports = [
+		./audio.nix
+		./desktop.nix
+		./pkgs.nix
+		./tex-editing-pkgs.nix
+	];
+}
