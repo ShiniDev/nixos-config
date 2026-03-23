af3fdb023685466951953c17cbbff6726aceaddb
shinidev <markjoefreylaurente@gmail.com>
2026-03-23 18:28:07 -0400

    added options on desktop.nix

--- modules/desktop/desktop.nix
+++ modules/desktop/desktop.nix
@@ -1,12 +1,29 @@
-{ ... }:
+{ lib, config, ... }:
 {
-  services.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
+  options.shinidev.desktop = {
+    windowManager = lib.mkOption {
+      type = lib.types.enum [
+        "sway"
+        "hyprland"
+      ];
+      default = "hyprland";
+      description = "The window manager config to use.";
+    };
   };
-  programs.hyprland = {
-    enable = true;
-    withUWSM = true;
-    xwayland.enable = true;
-  };
+  config = lib.mkMerge [
+    (lib.mkIf (config.shinidev.desktop.windowManager == "sway") {
+
+    })
+    (lib.mkIf (config.shinidev.desktop.windowManager == "hyprland") {
+      services.displayManager.sddm = {
+        enable = true;
+        wayland.enable = true;
+      };
+      programs.hyprland = {
+        enable = true;
+        withUWSM = true;
+        xwayland.enable = true;
+      };
+    })
+  ];
 }
