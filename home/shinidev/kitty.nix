9bd712909da2a427d94c81ee3e0ef555eb98c497
shinidev <markjoefreylaurente@gmail.com>
2026-03-23 14:19:54 -0400

    User kitty config

---
+++ home/shinidev/kitty.nix
@@ -0,0 +1,15 @@
+{
+  ...
+}:
+{
+  programs.kitty = {
+    enable = true;
+    settings = {
+      scrollback_lines = 10000;
+      enable_audio_bell = false;
+      update_check_interval = 0;
+      background_opacity = 0.40;
+      background_blur = 5;
+    };
+  };
+}
