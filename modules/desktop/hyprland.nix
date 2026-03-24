9148b2c1a0a7236a3b7f9fc09658cfafa00573cb
shinidev <markjoefreylaurente@gmail.com>
2026-03-24 07:04:52 -0400

    hyprland.nix (unused), added tree

---
+++ modules/desktop/hyprland.nix
@@ -0,0 +1,13 @@
+{ ... }:
+{
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    xwayland.enable = true;
+  };
+
+}
