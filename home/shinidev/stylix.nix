51a7381d5409079769cda8c3cd3dc691074c8821
shinidev <markjoefreylaurente@gmail.com>
2026-03-30 07:09:15 -0400

    Auto style mako and wofi

--- home/shinidev/stylix.nix
+++ home/shinidev/stylix.nix
@@ -3,4 +3,6 @@
   stylix.targets.kitty.enable = true;
   stylix.targets.starship.enable = true;
   stylix.targets.sway.enable = true;
+  stylix.targets.mako.enable = true;
+  stylix.targets.wofi.enable = true;
 }
