fb8be0a8922cd4f7c7a378cad8fd3519fc157b87
shinidev <markjoefreylaurente@gmail.com>
2026-04-05 22:11:23 -0400

    Moved out some nix files out of modules dir

--- flake.nix
+++ flake.nix
@@ -36,34 +36,13 @@
     };
   };
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
-
-  /*
-    outputs =
-    {
-      ...
-    }@inputs:
-    let
-      consts = import ./lib/consts.nix {
-        inherit inputs;
-      };
-    in
-    {
-      nixosConfigurations.home-station = inputs.nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs consts; };
-        system = consts.systems.default;
-        modules = consts.desktopModulesDefaults ++ [
-          ./hosts/home-station/configuration.nix
-          { home-manager.users.shinidev = ./home/shinidev/home.nix; }
-        ];
-      };
-      nixosConfigurations.home-server = inputs.nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs consts; };
-        system = consts.systems.default;
-        modules = consts.systemModulesDefaults ++ [
-          ./hosts/home-server/configuration.nix
-        ];
-      };
-    };
-  */
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      inputs.import-tree [
+        ./modules
+        ./lib
+        ./hosts
+      ]
+    );
 }
--- lib/consts.nix
+++ lib/consts.nix
@@ -1,22 +1,24 @@
 { inputs, ... }:
-rec {
-  systems = {
-    default = "x86_64-linux";
+{
+  flake.consts = rec {
+    systems = {
+      default = "x86_64-linux";
+    };
+    systemModulesDefaults = [
+      inputs.disko.nixosModules.disko
+      inputs.stylix.nixosModules.stylix
+      inputs.agenix.nixosModules.default
+      { environment.systemPackages = [ inputs.agenix.packages.${systems.default}.default ]; }
+    ];
+    homeManagerDefaults = {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.backupFileExtension = "backup";
+      home-manager.extraSpecialArgs = { inherit inputs; };
+    };
+    desktopModulesDefaults = systemModulesDefaults ++ [
+      inputs.home-manager.nixosModules.home-manager
+      homeManagerDefaults
+    ];
   };
-  systemModulesDefaults = [
-    inputs.disko.nixosModules.disko
-    inputs.stylix.nixosModules.stylix
-    inputs.agenix.nixosModules.default
-    { environment.systemPackages = [ inputs.agenix.packages.${systems.default}.default ]; }
-  ];
-  homeManagerDefaults = {
-    home-manager.useGlobalPkgs = true;
-    home-manager.useUserPackages = true;
-    home-manager.backupFileExtension = "backup";
-    home-manager.extraSpecialArgs = { inherit inputs; };
-  };
-  desktopModulesDefaults = systemModulesDefaults ++ [
-    inputs.home-manager.nixosModules.home-manager
-    homeManagerDefaults
-  ];
 }
