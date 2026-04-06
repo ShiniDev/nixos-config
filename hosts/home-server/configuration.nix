9aaf55e02595f80be45623138c841e0cec79abf9
shinidev <markjoefreylaurente@gmail.com>
2026-04-05 22:38:08 -0400

    home-server host config is now modularized

--- hosts/home-server/configuration.nix
+++ hosts/home-server/configuration.nix
@@ -1,12 +1,13 @@
-{ pkgs, ... }:
+{ ... }:
 {
-  imports = [
-    ./hardware-configuration.nix
-    ./disk-config.nix
-    ../../modules/system/_imports.nix
-  ];
-  system.stateVersion = "25.11"; # DO NOT CHANGE
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  networking.hostName = "home-server"; # Define your hostname.
-  shinidev.networking.role = "server";
+  flake.nixosModules.home-server =
+    { pkgs, ... }:
+    {
+      imports = [
+      ];
+      system.stateVersion = "25.11"; # DO NOT CHANGE
+      boot.kernelPackages = pkgs.linuxPackages_latest;
+      networking.hostName = "home-server"; # Define your hostname.
+      shinidev.networking.role = "server";
+    };
 }
