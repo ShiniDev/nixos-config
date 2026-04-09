9eed3fac56fcc7fbb7c5288595897cccb81821fe
shinidev <markjoefreylaurente@gmail.com>
2026-04-09 18:01:04 -0400

    Added home-server flake

--- hosts/home-server/configuration.nix
+++ hosts/home-server/configuration.nix
@@ -1,5 +1,19 @@
-{ ... }:
+{ inputs, self, ... }:
 {
+  flake.nixosConfigurations.home-server = inputs.nixpkgs.lib.nixosSystem {
+    specialArgs = { inherit inputs; };
+    system = "x86_64-linux";
+    modules =
+      self.consts.systemModulesDefaults
+      ++ (with self.nixosModules; [
+        home-server
+        home-server-hardware
+        core
+        apps
+        networking
+      ]);
+  };
+
   flake.nixosModules.home-server =
     { pkgs, ... }:
     {
@@ -8,6 +22,5 @@
       system.stateVersion = "25.11"; # DO NOT CHANGE
       boot.kernelPackages = pkgs.linuxPackages_latest;
       networking.hostName = "home-server"; # Define your hostname.
-      shinidev.networking.role = "server";
     };
 }
