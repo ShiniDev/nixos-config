e7e722cad8c367d7a87dad0ac648adc37b4d6ad5
shinidev <markjoefreylaurente@gmail.com>
2026-05-02 22:29:48 -0400

    Added nginx for ease of passing modpack zip files around the house.

--- modules/sandbox.nix
+++ modules/sandbox.nix
@@ -8,5 +8,18 @@
         unzip
         tmux
       ];
+      services.nginx = {
+        enable = true;
+        virtualHosts = {
+          localhost = {
+            root = "/srv/nginx";
+            locations = {
+              "/" = {
+                extraConfig = "autoindex on;";
+              };
+            };
+          };
+        };
+      };
     };
 }
